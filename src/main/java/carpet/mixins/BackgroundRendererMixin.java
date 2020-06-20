7e5328126d34083a30374e74a8ff2311e16a6f34
JohanVonElectrum <joanestradium@gmail.com>
2020-06-21 01:06:05 +0200

    Imports inecesarios eliminados

--- src/main/java/carpet/mixins/BackgroundRendererMixin.java
+++ src/main/java/carpet/mixins/BackgroundRendererMixin.java
@@ -1,7 +1,6 @@
 package carpet.mixins;
 
 import carpet.commands.FogCommand;
-import carpet.script.CarpetEventServer;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -17,10 +16,8 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(net.minecraft.client.render.BackgroundRenderer.class)
